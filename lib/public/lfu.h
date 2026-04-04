5b0b993b2eaef1bf7b343c95857f8f45604e83f4
Michal Brodziak <niesmialymichal@gmail.com>
2026-04-04 21:44:16 +0200

    implement lfu

--- lib/public/lfu.h
+++ lib/public/lfu.h
@@ -1,12 +1,41 @@
 #include "evict.h"
+#include <list>
+#include <unordered_map>
 
 namespace cash {
 
 class LFU {
+  std::unordered_map<int, std::list<EntryBase*>> _frequencies;
+  std::unordered_map<EntryBase*, int> _nodesFrequencies;
+  std::unordered_map<EntryBase*, std::list<EntryBase*>::iterator> _iterators;
+  int _min{0};
+
 public:
-  EntryBase* getVictim() { return nullptr; }
-  void onGet(EntryBase*) {}
-  void onPut(EntryBase*) {};
+  EntryBase* getVictim() {
+    EntryBase* victim = _frequencies[_min].back();
+    _frequencies[_min].erase(_iterators[victim]);
+    _nodesFrequencies.erase(victim);
+    _iterators.erase(victim);
+    return victim;
+  }
+
+  void onGet(EntryBase* node) {
+    int currentFreq = _nodesFrequencies[node]++;
+    _frequencies[currentFreq].erase(_iterators[node]);
+    // potentially update min if empty
+    if (_frequencies[currentFreq].empty() && currentFreq == _min)
+      _min++;
+
+    _frequencies[_nodesFrequencies[node]].emplace_front(node);
+    _iterators[node] = _frequencies[_nodesFrequencies[node]].begin();
+  }
+
+  void onPut(EntryBase* node) {
+    _nodesFrequencies.insert({node, 1});
+    _frequencies[1].emplace_front(node);
+    _iterators[node] = _frequencies[1].begin();
+    _min = 1;
+  };
 };
 
 } // namespace cash
